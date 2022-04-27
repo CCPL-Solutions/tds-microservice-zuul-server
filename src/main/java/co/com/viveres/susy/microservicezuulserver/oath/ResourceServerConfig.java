b2a05a9dfd7365e96053a44b39b76e5b726eec24
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2022-04-27 17:36:37 -0500

    primer commit

---
+++ src/main/java/co/com/viveres/susy/microservicezuulserver/oath/ResourceServerConfig.java
@@ -0,0 +1,84 @@
+package co.com.viveres.susy.microservicezuulserver.oath;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@RefreshScope
+@Configuration
+//@EnableResourceServer
+public class ResourceServerConfig{// extends ResourceServerConfigurerAdapter {
+
+	/**private static final String ENDPOINT_BASE_USERS = "/api/users/**";
+	private static final String ROLE_ADMIN = "ADMIN";
+	
+	@Value("${config.security.oauth.jwt.key}")
+	private String jwtKey;
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore());
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/api/security/**").permitAll()
+			// Microservice Users
+			.antMatchers(HttpMethod.POST, ENDPOINT_BASE_USERS).hasRole(ROLE_ADMIN)
+			.antMatchers(HttpMethod.GET, ENDPOINT_BASE_USERS).permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.cors().configurationSource(corsConfigurationSource());
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("POST","GET","PUT","DELTE","OPTIONS"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(this.corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	} 
+
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey(this.jwtKey);
+		return accessTokenConverter;
+	}**/
+
+}
---
+++ src/test/java/co/com/viveres/susy/microservicezuulserver/MicroserviceZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.viveres.susy.microservicezuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
