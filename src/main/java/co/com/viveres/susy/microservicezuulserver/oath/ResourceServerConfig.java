34460b274b0e5ce18c23bb8edf76a616f2406cc0
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2022-05-29 14:57:34 -0500

    Seguridad , sleuth y zipkin

--- src/main/java/co/com/viveres/susy/microservicezuulserver/oath/ResourceServerConfig.java
+++ src/main/java/co/com/viveres/susy/microservicezuulserver/oath/ResourceServerConfig.java
@@ -9,12 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-//import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,10 +22,10 @@ import org.springframework.web.filter.CorsFilter;
 
 @RefreshScope
 @Configuration
-//@EnableResourceServer
-public class ResourceServerConfig{// extends ResourceServerConfigurerAdapter {
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	/**private static final String ENDPOINT_BASE_USERS = "/api/users/**";
+	private static final String ENDPOINT_BASE_USERS = "/api/users/**";
 	private static final String ROLE_ADMIN = "ADMIN";
 	
 	@Value("${config.security.oauth.jwt.key}")
@@ -79,6 +79,6 @@ public class ResourceServerConfig{// extends ResourceServerConfigurerAdapter {
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
 		accessTokenConverter.setSigningKey(this.jwtKey);
 		return accessTokenConverter;
-	}**/
+	}
 
 }
